44bee249ef075a4b31be960e4389f0d588aedf93
agustinc3 <hcagnoli@udesa.edu.ar>
2022-11-13 16:45:22 -0300

    funcionalidad favoritos

--- js/detallepeli.js
+++ js/detallepeli.js
@@ -1,8 +1,10 @@
+//buscar id
 let qs = location.search;
 let objetoqs = new URLSearchParams(qs);
 let idPeli = objetoqs.get("buscador")
 idPeli = '550'
 
+//sacar la info dinamica del html
 let titulo = document.querySelector(".pelicula")
 let sinopsis = document.querySelector(".sinopsis")
 let estreno = document.querySelector(".infogeneral .infocostado #estreno")
@@ -10,8 +12,9 @@ let duracion = document.querySelector(".infogeneral .infocostado #duracion")
 let imagen = document.querySelector(".infogeneral .portada")
 let generos = document.querySelector(".favorito #genero")
 let valoracion = document.querySelector(".infocostado #rating")
+let fav=document.querySelector("i")
 
-
+//api
 let url = `https://api.themoviedb.org/3/movie/${idPeli}?api_key=062d3ce7ea9819367da90034f22a527a&language=en-US`;
 
 fetch(url)
@@ -20,19 +23,20 @@ fetch(url)
     })
     .then(function (data) {
         console.log(data);
+        //modificar datos segun el id
         titulo.innerText = data.original_title;
         sinopsis.innerText = data.overview;
         estreno.innerText = 'Release: ' + data.release_date;
         duracion.innerText = data.runtime + ' Minutes'
         valoracion.innerText = data.vote_average * 10 + '/100'
 
-
+        //buscar portada
         let portada = []
         foto_ruta = data.poster_path
         console.log(foto_ruta);
         portada += `<img src="https://image.tmdb.org/t/p/w500${foto_ruta}" id="foto-shrek" alt="portada">`
 
-
+        //crear un array de generos para ponerlos en display
         let qGeneros = data.genres
 
         let genres = []
@@ -42,6 +46,7 @@ fetch(url)
             genres += `<a href="./detallegenero.html?search_query=${idGenero}"> ${nombreGenero} </a>`
         }
 
+        //mostrar imagen y generos
         generos.innerHTML = genres
         imagen.innerHTML = portada
         return data;
@@ -50,7 +55,7 @@ fetch(url)
         return error;
     });
 
-
+//donde se encuentra la pelicula
 URLproveedor = `https://api.themoviedb.org/3/movie/${idPeli}/watch/providers?api_key=062d3ce7ea9819367da90034f22a527a`
 
 fetch(URLproveedor)
@@ -58,6 +63,7 @@ fetch(URLproveedor)
         return response.json();
     }).then(function (data) {
         console.log(data);
+        //sacar info de la api
         let sitios = data.results.AR.flatrate;
         
         console.log(sitios);
@@ -65,11 +71,12 @@ fetch(URLproveedor)
         let plataforma = document.querySelector(".verEn")
         let todosProveedores = [];
         let lista_proveedores = []
-
+        //crear array con proveedores
         for (let i = 0; i < sitios.length; i++) {
             todosProveedores[i] =sitios[i].provider_name;
         }
         console.log(todosProveedores);
+        //lo que va en el html
         for (let i = 0; i < todosProveedores.length; i++) {
             lista_proveedores += `<li>${todosProveedores[i]}</li>`
         }
@@ -81,3 +88,33 @@ fetch(URLproveedor)
         console.log(error);
         return error;
     });
+
+
+//agregando favoritos    
+let favoritos=[]
+
+let recuperoStorage = localStorage.getItem("favoritos")
+//si no hay favoritos
+if (recuperoStorage != null) {
+    favoritos =  JSON.parse(recuperoStorage)
+}
+//si la pelicula ya esta en favoritos
+if (favoritos.includes(idPeli)) {
+    fav.innerText = "Quitar de favoritos";
+}
+//si se clickea en favoritos
+fav.addEventListener("click", function(e) {
+    e.preventDefault();
+    //si esta o no
+    if (favoritos.includes(idPeli)) {
+       let indice = favoritos.indexOf(idPeli)
+       favoritos.splice(indice, 1);
+       fav.innerText = "Agregar a Fav";
+    }else{
+        favoritos.push(idPeli)
+        fav.innerText = "Quitar de favoritos"
+    }
+    //para que quede guardado
+    let favsToString = JSON.stringify(favoritos);
+    localStorage.setItem("favoritos", favsToString )
+})
