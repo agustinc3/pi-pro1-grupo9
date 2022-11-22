01a429f91f86463b90bafe8ccc0057cce58cdd51
nspagnuolo@udesa.edu.ar <nicolasspagnuolo@Nicolass-MBP.fibertel.com.ar>
2022-11-22 19:17:57 -0300

    hola

--- js/index.js
+++ js/index.js
@@ -5,9 +5,9 @@ form.addEventListener('submit', function(event) {
     event.preventDefault();
 
     if (busqueda.value == "") {
-        alert('No puedes enviar el form vacio');
+        alert('No podes enviar el form vacio');
     } else if(busqueda.value.length <= 3){
-        alert('Debes escribir mas de 3 caracteres');
+        alert('Tenes que escribir mas de 3 caracteres');
     } else {
         this.submit();
     }
