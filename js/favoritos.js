43f0fa4fc39614266f2049e1ff859c7fbb944499
nspagnuolo@udesa.edu.ar <nicolasspagnuolo@Nicolass-MBP.fibertel.com.ar>
2022-11-23 22:12:04 -0300

    hola

--- js/favoritos.js
+++ js/favoritos.js
@@ -4,13 +4,14 @@ let recuperoStorage = localStorage.getItem("favoritos");
 let favoritos = JSON.parse(recuperoStorage);
 
 let section = document.querySelector('.fila');
+let peliculasFavoritas=[]
 
 
 
 if (favoritos == null || favoritos.length == 0) {
     section.innerHTML = '<p>No hay datos en favoritos<p/>'
 } else {
-    for (let i = 0; i < favoritos.length; i++) {
+    for (let i = 1; i < favoritos.length; i++) {
         let idPeli=favoritos[i]
         console.log(idPeli)
         let url = `https://api.themoviedb.org/3/movie/${idPeli}?api_key=062d3ce7ea9819367da90034f22a527a&language=en-US`;
@@ -20,13 +21,14 @@ if (favoritos == null || favoritos.length == 0) {
             return respuesta.json();
         })
         .then(function(data) {
-            let peliculasFavoritas=[]
+            
+            console.log(data)
 
-        peliculasFavoritas += `<a href="./detalle.html?buscador=${data.id}"><article>
-                                        <img src=${data.image} alt='${data.name}' />
-                                        <p>Name: ${data.name} </p>
-                                        <p>Status: ${data.status} </p>
-                                    </article></a>`
+        peliculasFavoritas += `<a href="./detallepeli.html?search_query=${data.id}"><article>
+                                        <img src="https://image.tmdb.org/t/p/w500${data.poster_path}"/>
+                                        <p style="color:black">Name: ${data.original_title} </p>
+                                        </article>
+                                    </a>`
             section.innerHTML =peliculasFavoritas;
             return data;
         })
